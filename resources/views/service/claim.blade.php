60aa18efad7fe7e9c0628cb9ea1f64110cb7e7d8
Kojo Yeboah <kojo53i@live.com>
2021-11-17 16:11:24 +0000

    update

--- resources/views/service/claim.blade.php
+++ resources/views/service/claim.blade.php
@@ -29,57 +29,33 @@
                     <div class="container text-center mx-auto" style="width: 50%">
                         <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                             <!-- Replace with your content -->
-                            <div class="px-4 py-6 sm:px-0">
+                            <div class=" text-2xl text-white mb-5">
+                                    <h2>Search with patient's policy no.</h2>
+                            </div>
+                            {{-- <div class="px-4 py-6 sm:px-0"> --}}
+                                <div>
+
                               <form action="{{ route('create.partner')}}" method="POST" id="add-partner-form">
                                 @csrf
-                                  <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                      <label for="inputEmail4">Email</label>
-                                      <input type="email" class="form-control" id="inputEmail4">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                      <label for="inputPassword4">Password</label>
-                                      <input type="password" class="form-control" id="inputPassword4">
-                                    </div>
-                                  </div>
-                                  <div class="form-group">
-                                    <label for="inputAddress">Address</label>
-                                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                                  </div>
-                                  <div class="form-group">
-                                    <label for="inputAddress2">Address 2</label>
-                                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                                  </div>
-                                  <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                      <label for="inputCity">City</label>
-                                      <input type="text" class="form-control" id="inputCity">
-                                    </div>
-                                    <div class="form-group col-md-4">
-                                      <label for="inputState">State</label>
-                                      <select id="inputState" class="form-control">
-                                        <option selected>Choose...</option>
-                                        <option>...</option>
-                                      </select>
-                                    </div>
-                                    <div class="form-group col-md-2">
-                                      <label for="inputZip">Zip</label>
-                                      <input type="text" class="form-control" id="inputZip">
-                                    </div>
-                                  </div>
-                                  <div class="form-group">
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="checkbox" id="gridCheck">
-                                      <label class="form-check-label" for="gridCheck">
-                                        Check me out
-                                      </label>
-                                    </div>
-                                  </div>
-                                  <button type="submit" class="btn btn-primary">Sign in</button>
-                              </form>
+                                  {{-- <div class="form-group w-full"> --}}
+                                    <div class="form-group">
+                                      <div class="flex">
+                                      <input type="text" class="form-control" name="policyNumber" id="policyNumber" placeholder="policy number">
+                                      <button class="flex items-center py-1 px-4 bg-green-400 rounded"> 
+                                        <i class="material-icons-round text-base text-white">search</i>
+                                      </button>
+                                </div>
+                                </div>
+
+                       
+
+                                {{-- <div class="w-1/2 text-center mx-auto">
+                                  <button type="submit" class="py-2 text-white text-2xl btn btn-block bg-green-400 hover:bg-green-500">Search</button>
+                                </div> --}}
+                             </form>
                             </div>
                             <!-- /End replace -->
-                          </div>
+                          {{-- </div> --}}
 
                     </div>
                   </div>
