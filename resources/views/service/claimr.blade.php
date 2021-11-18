e984755ea6d690990862d17d630138a468de5a29
Kojo Yeboah <kojo53i@live.com>
2021-11-18 11:49:32 +0000

    update

--- resources/views/service/claimr.blade.php
+++ resources/views/service/claimr.blade.php
@@ -17,6 +17,8 @@
                   
               <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 flex-col">
                     policy number
+             @if(!empty($subscribers))  
+
               </div>
               </main>
 @endsection
