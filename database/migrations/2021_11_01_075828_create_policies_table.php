f0c9fdbd17efacd776f7f70d4e815318770b56a5
Kojo Yeboah <kojo53i@live.com>
2021-11-18 09:41:38 +0000

    update

--- database/migrations/2021_11_01_075828_create_policies_table.php
+++ database/migrations/2021_11_01_075828_create_policies_table.php
@@ -15,7 +15,11 @@ class CreatePoliciesTable extends Migration
     {
         Schema::create('policies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('policy_number');
+            $table->string('medical_condition');
+            $table->string('total_cover_balance');
+            $table->unsignedInteger('partner_id');
+            $table->foreign('partner_id')->references('id')->on('partners')->onDelete('cascade');
             $table->timestamps();
         });
     }
