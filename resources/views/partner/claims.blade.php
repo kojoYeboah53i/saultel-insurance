98295dad3fc2cde2eabd9f669923b72b2f6ac372
Kojo Yeboah <kojo53i@live.com>
2021-11-19 08:14:32 +0000

    update

--- resources/views/partner/claims.blade.php
+++ resources/views/partner/claims.blade.php
@@ -18,30 +18,35 @@
               <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 flex-col">
                 <div class="row">
                     <div class="col">
-                      <div class="card bg-info">
-                        <div class="card-body">
-                            <div class="flex justify-between mb-5">
-                                <span class="font-extrabold text-lg">Total Number of Subscribers</span>
-                                <span class="">15%</span>
-                            </div>
-                    
-            
-                          <div class="flex justify-between mt-5">
-                            <i class="fas fa-user fa-2x"></i>
-                            <span class="users flex ">
-                                145,000</span>
-                                <span class="badge badge-light">April</span>
-                             
-                        </div>
-            
+                        <div class="card bg-info">
+                          <div class="card-body">
+                              <div class="flex justify-between mb-5">
+                                  <span class="font-extrabold text-lg">Total Number of Subscribers 
+                                      <span style="visibility: hidden">hail</span>
+                                      <span class="hide" style="visibility: hidden">for this</span>
+                                    </span>
+                                  <span class="">15%</span>
+                              </div>
+                      
+              
+                            <div class="flex justify-between mt-5">
+                              <i class="fas fa-user fa-2x"></i>
+                              <span class="users flex ">
+                                  145,000</span>
+                                  <span class="badge badge-light">April</span>
+                               
+                          </div>
+              
+                          </div>
                         </div>
                       </div>
-                    </div>
                     <div class="col rounded-2xl">
                       <div class="card btn-success">
                         <div class="card-body">
                             <div class="flex justify-between mb-5">
-                                <span class="font-extrabold text-lg">Total Paid Claims <br><span class="hide">for this</span></span>
+                                <span class="font-extrabold text-lg">Total Paid Claims <br>
+                                    <span class="hide" style="visibility: hidden">for this</span>
+                                </span>
                                 <span class="">22%</span>
                             </div>
                     
@@ -61,7 +66,10 @@
                         <div class="card bg-warning">
                             <div class="card-body">
                                 <div class="flex justify-between mb-5">
-                                    <span class="font-extrabold text-lg">Total Pending Claim Request</span>
+                                    <span class="font-extrabold text-lg">
+                                        Total Pending Claim Request</span>
+                                    <span class="hide" style="visibility: hidden">for this</span>
+
                                     <span class="">5%</span>
                                 </div>
                         
@@ -80,9 +88,33 @@
           
                    
                   </div>
-                  <h1>
-                      claims page
-                  </h1>
+                  <div class="w-full mt-10">
+                    {{-- <div class="col-xl-9 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing"> --}}
+                        <div class="widget widget-chart-three">
+                            <div class="widget-heading">
+                                <div class="">
+                                    <h5 class="">Paid Claims Vs Declined</h5>
+                                </div>
+        
+                                <div class="dropdown  custom-dropdown">
+                                    <a class="dropdown-toggle" href="#" role="button" id="uniqueVisitors" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
+                                    </a>
+        
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="uniqueVisitors">
+                                        <a class="dropdown-item" href="javascript:void(0);">View</a>
+                                        <a class="dropdown-item" href="javascript:void(0);">Update</a>
+                                        <a class="dropdown-item" href="javascript:void(0);">Download</a>
+                                    </div>
+                                </div>
+                            </div>
+        
+                            <div class="widget-content">
+                                <div id="uniqueVisits"></div>
+                            </div>
+                        </div>
+                    {{-- </div> --}}
+                  </div>
                 <div class="widget-content mt-10">
                     <div class="table-responsive">
                         <table class="table">
@@ -154,4 +186,9 @@
                 </div>
               </div>
                 </main>
+
+                <link href="{{asset('plugins/apex/apexcharts.css')}}" rel="stylesheet" type="text/css">
+                <link href="{{asset('assets/css/dashboard/dash_2.css')}}" rel="stylesheet" type="text/css" />
+                <script src="{{asset('plugins/apex/apexcharts.min.js')}}"></script>
+                <script src="{{asset('assets/js/dashboard/subscribers.js')}}"></script>
 @endsection
--- resources/views/partner/subscription.blade.php
+++ resources/views/partner/subscription.blade.php
@@ -91,33 +91,7 @@
           
                    
                   </div>
-                  <div class="w-full mt-10">
-                    {{-- <div class="col-xl-9 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing"> --}}
-                        <div class="widget widget-chart-three">
-                            <div class="widget-heading">
-                                <div class="">
-                                    <h5 class="">Paid Claims Vs Declined</h5>
-                                </div>
-        
-                                <div class="dropdown  custom-dropdown">
-                                    <a class="dropdown-toggle" href="#" role="button" id="uniqueVisitors" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                    </a>
-        
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="uniqueVisitors">
-                                        <a class="dropdown-item" href="javascript:void(0);">View</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Update</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Download</a>
-                                    </div>
-                                </div>
-                            </div>
-        
-                            <div class="widget-content">
-                                <div id="uniqueVisits"></div>
-                            </div>
-                        </div>
-                    {{-- </div> --}}
-                  </div>
+     
                 <div class="widget-content mt-10">
                     <span class="text-white font-bold text-3xl mb-10">
                         Recent Transactions
@@ -258,9 +232,6 @@
               </div>
                 </main>
 
-<link href="{{asset('plugins/apex/apexcharts.css')}}" rel="stylesheet" type="text/css">
-<link href="{{asset('assets/css/dashboard/dash_2.css')}}" rel="stylesheet" type="text/css" />
-<script src="{{asset('plugins/apex/apexcharts.min.js')}}"></script>
-<script src="{{asset('assets/js/dashboard/subscribers.js')}}"></script>
+
 {{-- <script src="{{asset('assets/js/dashboard/service.js')}}"></script> --}}
 @endsection
